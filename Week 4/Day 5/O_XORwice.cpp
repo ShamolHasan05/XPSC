01d673b2dff2072876eb3a56ee0712e8f2da5c63
ShamolHasan05 <153484163+ShamolHasan05@users.noreply.github.com>
2024-04-16 22:51:29 +0600

    Add files via upload
    day 5 added

---
+++ Week 4/Day 5/O_XORwice.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define newline cout<<'\n';
+using namespace std;
+
+void solve(){
+    int a,b;
+    cin>>a>>b;
+    int ans = a^b;
+    cout<<ans; newline
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+    
+    return 0;
+}
