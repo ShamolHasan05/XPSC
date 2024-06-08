bbb63bce5ff6690203df6fdea5817bd593142612
ShamolHasan05 <153484163+ShamolHasan05@users.noreply.github.com>
2024-06-08 22:56:17 +0600

    Add files via upload
    add

---
+++ Week 12/Day 1/A_Increasing_Sequence.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define gcd(a,b) __gcd(a, b)
+#define lcm(a,b) ((a*b)/gcd(a,b))
+#define newline cout<<'\n';
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    ll ans = 0;
+    for(int i=0; i<n; i++){
+        ans++;
+        if(ans == a[i]){
+            ans++;
+        }
+    }
+
+    cout<<ans; newline
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+    
+    return 0;
+}
