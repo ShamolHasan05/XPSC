630233c10cec0c6c635b25fbbdc17254269e2dff
ShamolHasan05 <153484163+ShamolHasan05@users.noreply.github.com>
2024-07-01 22:00:52 +0600

    Add files via upload
    add

---
+++ Week 14/Day 3/D_Same_Differences.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define yes cout<<"YES"<<'\n';
+#define no cout<<"NO"<<'\n';
+#define gcd(a,b) __gcd(a, b)
+#define lcm(a,b) ((a*b)/gcd(a,b))
+#define newline cout<<'\n';
+using namespace std;
+
+void solve(){
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0; i<n; i++){
+        cin>>a[i];
+    }
+    ll ans = 0;
+    int k = 1;
+    map<int, int> mp;
+    for(int val : a){
+        ans += mp[val - k]++;
+        k++;
+    }
+    cout<<ans; newline
+
+}
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+
+    
+    return 0;
+}
